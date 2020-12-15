7516cd87bda992e12fce8c60d07f5c0c3b6dae51
freemt <yucongo+fmt@gmail.com>
2020-12-15 11:19:36 +0800

    update version 0.1.1

--- hlm_texts/__init__.py
+++ hlm_texts/__init__.py
@@ -2,8 +2,8 @@
 from pathlib import Path
 from .load_text import load_text
 
-__version__ = '0.1.0'
+__version__ = "0.1.1"
 
 cdir = Path(__file__).parent
 hlm_zh = load_text(cdir / "340-脂砚斋重批红楼梦.txt")
-hlm_en =  load_text(cdir / "david-hawks-the-story-of-the-stone.txt")
+hlm_en = load_text(cdir / "david-hawks-the-story-of-the-stone.txt")
