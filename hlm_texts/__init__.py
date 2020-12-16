bbfd49e902ecbfa4c5d1f5d678fcec07ef83f719
freemt <yucongo+fmt@gmail.com>
2020-12-16 16:58:50 +0800

    update

--- hlm_texts/__init__.py
+++ hlm_texts/__init__.py
@@ -8,6 +8,8 @@ __version__ = "0.1.1"
 cdir = Path(__file__).parent
 hlm_zh = load_text(cdir / "340-脂砚斋重批红楼梦.txt")
 hlm_en = load_text(cdir / "david-hawks-the-story-of-the-stone.txt")
+hlm_en1 = load_text(cdir / "yang-hlm.txt")
+hlm_en2 = load_text(cdir / "joly-hlm.txt")
 
 fangfang_en = load_text(cdir / "fangfang-en.txt")
 fangfang_zh = load_text(cdir / "fangfang-zh.txt")
