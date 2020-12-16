56a9ff0a966544c3e20b86d2e4985a6ff53693c4
freemt <yucongo+fmt@gmail.com>
2020-12-16 14:00:21 +0800

    Update codacity styling

--- hlm_texts/__init__.py
+++ hlm_texts/__init__.py
@@ -4,6 +4,7 @@ from .load_text import load_text
 
 __version__ = "0.1.1"
 
+# pylint: disable=invalid-name
 cdir = Path(__file__).parent
 hlm_zh = load_text(cdir / "340-脂砚斋重批红楼梦.txt")
 hlm_en = load_text(cdir / "david-hawks-the-story-of-the-stone.txt")
