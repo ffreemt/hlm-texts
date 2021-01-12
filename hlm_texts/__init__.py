726cb40d1a088c51c6e223382d9335ebd5ec1b66
freemt <yucongo+fmt@gmail.com>
2021-01-12 21:29:37 +0800

    update version 0.1.6

--- hlm_texts/__init__.py
+++ hlm_texts/__init__.py
@@ -2,7 +2,8 @@
 from pathlib import Path
 from .load_text import load_text
 from .seg_text import seg_text
-from .sent_tokenizer import sent_tokenizer
+from .sent_tokenizer import sent_tokenizer, _sent_tokenizer
+from .gen_tokens import gen_tokens, list_tokens
 
 __version__ = "0.1.6"
 
@@ -28,4 +29,7 @@ __all__ = (
     "load_text",
     "seg_text",
     "sent_tokenizer",
+    "_sent_tokenizer",
+    "gen_tokens",
+    "list_tokens",
 )
--- hlm_texts/gen_tokens.py
+++ hlm_texts/gen_tokens.py
@@ -13,6 +13,9 @@ from typing import (
     Union,
 )
 
+import os
+from pathlib import Path
+from joblib import Memory
 from polyglot.text import Detector
 from polyglot.detect.base import logger as polyglot_logger
 from logzero import logger
@@ -21,17 +24,21 @@ from logzero import logger
 from hlm_texts.sent_tokenizer import _sent_tokenizer
 
 polyglot_logger.setLevel("ERROR")
+if os.environ.get("JLNOCACHE") is None:
+    _ = Path("~/joblib_cache").expanduser()
+else:
+    _ = None  # no cache if JLNOCACHE is set for testing
+memory = Memory(location=_, verbose=0)
 
 
 # fmt: off
-def gen_token(
+def gen_tokens(
         text: Union[str, List[str]],
         label: str = "",
         lang: Optional[str] = None,
-        gen_para: bool = False,
-        gen_sent: bool = True,
-        gen_phrase: bool = False,
-        # ) -> Generator[Tuple[str, str, int, str], None, None]:
+        gen_para: Union[int, bool] = False,
+        gen_sent: Union[int, bool] = True,
+        gen_phrase: Union[int, bool] = False,
 ) -> Iterator[Tuple[str, str, int, str]]:
     # fmt: on
     """Genereate tokens from text/list of text."""
@@ -52,6 +59,30 @@ def gen_token(
     if gen_phrase:
         for idx, para in enumerate(text):
             for sent in _sent_tokenizer(para, lang):
-                ...
+                raise Exception(
+                    "need to install phrase_tokenizer"
+                    "which is dependant of benepar"
+                )
                 # for phrase in phrase_tok(sent):
                 # yield phrase, label, idx + 1, 'phra'
+
+@memory.cache
+# fmt: off
+def list_tokens(
+        text: Union[str, List[str]],
+        label: str = "",
+        lang: Optional[str] = None,
+        gen_para: Union[int, bool] = False,
+        gen_sent: Union[int, bool] = True,
+        gen_phrase: Union[int, bool] = False,
+) -> List[Tuple[str, str, int, str]]:
+    # fmt: on
+    """List tokens with memoization."""
+    return [*gen_tokens(
+        text,
+        label,
+        lang,
+        gen_para,
+        gen_sent,
+        gen_phrase,
+    )]
